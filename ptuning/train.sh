6489ce4c745443690a190cb93cb8cf1013ba8844
mdzz <350304051@qq.com>
2023-12-27 16:38:35 +0800

    change finetune config

--- ptuning/train.sh
+++ ptuning/train.sh
@@ -4,14 +4,14 @@ NUM_GPUS=1
 
 torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_GPUS main.py \
     --do_train \
-    --train_file AdvertiseGen/train.json \
-    --validation_file AdvertiseGen/dev.json \
+    --train_file /content/ChatGLM2-6B/ptuning/self_data/train.json \
+    --validation_file /content/ChatGLM2-6B/ptuning/self_data/dev.json \
     --preprocessing_num_workers 10 \
     --prompt_column content \
     --response_column summary \
     --overwrite_cache \
-    --model_name_or_path THUDM/chatglm2-6b \
-    --output_dir output/adgen-chatglm2-6b-pt-$PRE_SEQ_LEN-$LR \
+    --model_name_or_path THUDM/chatglm2-6b-int4 \
+    --output_dir /content/drive/MyDrive/saved_model/adgen-chatglm2-6b-pt-$PRE_SEQ_LEN-$LR \
     --overwrite_output_dir \
     --max_source_length 64 \
     --max_target_length 128 \
@@ -19,9 +19,9 @@ torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_GPUS main.py \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 16 \
     --predict_with_generate \
-    --max_steps 3000 \
+    --max_steps 100 \
     --logging_steps 10 \
-    --save_steps 1000 \
+    --save_steps 10 \
     --learning_rate $LR \
     --pre_seq_len $PRE_SEQ_LEN \
     --quantization_bit 4
