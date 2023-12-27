5226e1fbc8bcb9686126c2215fc594d486d10627
mdzz <350304051@qq.com>
2023-12-27 16:44:19 +0800

    change config

--- ptuning/evaluate.sh
+++ ptuning/evaluate.sh
@@ -1,18 +1,18 @@
 PRE_SEQ_LEN=128
 CHECKPOINT=adgen-chatglm2-6b-pt-128-2e-2
-STEP=3000
+STEP=100
 NUM_GPUS=1
 
 torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_GPUS main.py \
     --do_predict \
-    --validation_file AdvertiseGen/dev.json \
-    --test_file AdvertiseGen/dev.json \
+    --validation_file /content/ChatGLM2-6B/ptuning/self_data/dev.json \
+    --test_file /content/ChatGLM2-6B/ptuning/self_data/dev.json \
     --overwrite_cache \
     --prompt_column content \
     --response_column summary \
     --model_name_or_path THUDM/chatglm2-6b \
-    --ptuning_checkpoint ./output/$CHECKPOINT/checkpoint-$STEP \
-    --output_dir ./output/$CHECKPOINT \
+    --ptuning_checkpoint /content/drive/MyDrive/saved_model/$CHECKPOINT/checkpoint-$STEP \
+    --output_dir /content/drive/MyDrive/saved_model/$CHECKPOINT \
     --overwrite_output_dir \
     --max_source_length 64 \
     --max_target_length 64 \
