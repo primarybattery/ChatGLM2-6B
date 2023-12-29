6bbef0f274b4378680c42b43952e1fab6e69dbb8
primarybattery <97868897+primarybattery@users.noreply.github.com>
2023-12-29 11:47:03 +0800

    Add files via upload

--- ptuning/train.sh
+++ ptuning/train.sh
@@ -17,11 +17,11 @@ torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_GPUS main.py \
     --max_target_length 128 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 8 \
+    --gradient_accumulation_steps 1 \
     --predict_with_generate \
-    --max_steps 20 \
-    --logging_steps 10 \
-    --save_steps 1 \
+    --max_steps 100 \
+    --logging_steps 1 \
+    --save_steps 2 \
     --learning_rate $LR \
     --pre_seq_len $PRE_SEQ_LEN \
     --quantization_bit 4
